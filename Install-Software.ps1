b7a5e7cf7e4bdc96019487b4d6b80d1fc005e389
Jan de Vries <jan@jan-v.nl>
2020-01-31 09:56:47 +0100

    Addes missing software to script and submodule for windows dev box

--- Install-Software.ps1
+++ Install-Software.ps1
@@ -10,54 +10,45 @@ choco install firefox -y
 refreshenv
 choco install googlechrome -y 
 refreshenv
-#choco install itunes -y 
-#refreshenv
-#choco install icloud -y #Should done later when Outlook is properly configured
-refreshenv
-#choco install crashplan -y 
-#refreshenv
 choco install slack  -y 
-#choco install inputdirector -y 
-#choco install paint.net -y  #Does not have checksums, discarded.
+refreshenv
+choco install microsoft-teams -y
+refreshenv
 choco install sublimetext3  -y 
+refreshenv
 choco install vlc  -y 
 refreshenv
 choco install adobereader -y
 refreshenv
-choco install rdcman -y
+choco install gpmdp -y
 refreshenv
-#choco install spotify 
+choco install snagit -y --params "/licensekey:ABCD-EFGH-IJKL /licensename:""Joe Bloggs"" /nodesktopshortcut"
+refreshenv
+choco install darktable -y
+choco install zoom -y
+choco install obs-studio -y
+choco install office365business -y
 refreshenv
 
 #Development software IDE
+choco install visualstudio2019enterprise -y --package-parameters "--add Microsoft.VisualStudio.Workload.Azure;includeRecommended;includeOptional --add Microsoft.VisualStudio.Workload.NetWeb;includeRecommended;includeOptional --addMicrosoft.VisualStudio.Workload.Node;includeRecommended;includeOptional"
 choco install visualstudiocode -y 
-#choco install visualstudio2015enterprise -y 
+choco install sublimetext3 -y
 
 #Development software tooling
 choco install beyondcompare -y 
-choco install fiddler4 -y 
+choco install fiddler -y 
 refreshenv
 #choco install resharper-platform -y #Gaat beter via de reguliere installer
 refreshenv
-choco install git.install -y 
+choco install git -y  --params "/GitAndUnixToolsOnPath /NoGitLfs /SChannel /NoAutoCrlf /WindowsTerminal /NoGuiHereIntegration /NoShellHereIntegration"
 refreshenv
 #choco install github  -y #Has wrong checksum
 refreshenv
-choco install sourcetree  -y 
-refreshenv
 choco install gitextensions  -y 
 refreshenv
-#choco install vmwareworkstation 
 choco install nodejs.install -y
 refreshenv
 choco install zoomit -y
-choco install python2  -y
+choco install python  -y
 refreshenv
-#choco install python  -y
-#refreshenv
-#choco install gitkraken -y
-#refreshenv
-#choco install filezilla -y
-#choco install openlivewriter -y
-#choco install teamviewer -y
-#choco install mousewithoutborders -y
